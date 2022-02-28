c18174c0f047d816cfd6525ef750004d9d63d6a4
pfg <pfg@pfg.pw>
2022-02-28 12:10:46 -0500

    .

---
+++ src/node_highlighter.tsx
@@ -0,0 +1,9 @@
+// export function highlightNode(node: )
+
+// parent, accessor, marker, initial
+
+// we need to watch on solidjs insertExpression and call
+// highlightNode with the bounding rect of the modified value
+
+// then we add a fixed positioned highlighter div to the body
+// and delete it after like 500ms or something
